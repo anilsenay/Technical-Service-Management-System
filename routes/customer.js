ee80906af019242058b4c005eb46ab402145e2ea
Anıl Şenay <anilsenay3@gmail.com>
2021-01-06 22:35:40 +0300

    Add query params

--- routes/customer.js
+++ routes/customer.js
@@ -19,6 +19,26 @@ router.get("/", (req, res) => {
   });
 });
 
+// Get customer's all repairments information with given firstName, lastName and phoneNumber parameters.
+router.get(encodeURI("/repairments"), (req, res) => {
+  console.log(req.query);
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("firstName", sql.NVarChar(50), req.query.firstName || "NULL");
+    request.input("lastName", sql.NVarChar(50), req.query.lastName || "NULL");
+    request.input(
+      "phoneNumber",
+      sql.NVarChar(20),
+      "+" + req.query.phoneNumber || "NULL"
+    );
+    request.execute("sp_getCustomerRepairments", (err, result) => {
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+      return res.send({ repairments: result.recordsets[0] });
+    });
+  });
+});
+
 // Get customer with given id.
 router.get("/:id", (req, res) => {
   sql.connect(sqlConfig, () => {
@@ -36,23 +56,5 @@ router.get("/:id", (req, res) => {
     );
   });
 });
-
-// Get customer's all repairments information with given firstName, lastName and phoneNumber parameters.
-router.get(
-  encodeURI("/repairments/:firstName/:lastName/:phoneNumber"),
-  (req, res) => {
-    sql.connect(sqlConfig, () => {
-      var request = new sql.Request();
-      request.input("firstName", sql.NVarChar(50), req.params.firstName);
-      request.input("lastName", sql.NVarChar(50), req.params.lastName);
-      request.input("phoneNumber", sql.NVarChar(20), req.params.phoneNumber);
-      request.execute("sp_getCustomerRepairments", (err, result) => {
-        res.setHeader("Content-Type", "application/json");
-        sql.close();
-        return res.send({ repairments: result.recordsets[0] });
-      });
-    });
-  }
-);
 
 module.exports = router;
