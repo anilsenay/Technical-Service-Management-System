7b7e24301ed175739001822c6ecec72adc22b476
bilgehangecici <bilgehangecici@gmail.com>
2021-01-06 21:20:55 +0300

    Update customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -37,4 +37,22 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// Get customer's all repairments information with given firstName, lastName and phoneNumber parameters.
+router.get(
+  encodeURI("/repairments/:firstName/:lastName/:phoneNumber"),
+  (req, res) => {
+    sql.connect(sqlConfig, () => {
+      var request = new sql.Request();
+      request.input("firstName", sql.NVarChar(50), req.params.firstName);
+      request.input("lastName", sql.NVarChar(50), req.params.lastName);
+      request.input("phoneNumber", sql.NVarChar(20), req.params.phoneNumber);
+      request.execute("sp_getCustomerRepairments", (err, result) => {
+        res.setHeader("Content-Type", "application/json");
+        sql.close();
+        return res.send({ repairments: result.recordsets[0] });
+      });
+    });
+  }
+);
+
 module.exports = router;
