4ddeb0b00304160d144348622536c2701c429b23
bilgehangecici <bilgehangecici@gmail.com>
2021-01-06 20:16:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ path: `${__dirname}/.env` });
 
 var sqlConfig = {
   server: `${process.env.SERVER}`,
   database: `${process.env.DATABASE}`,
-  user: `${process.env.USERNAME}`,
-  password: `${process.env.PASSWORD}`,
+  user: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
 };
 
 module.exports = sqlConfig;
