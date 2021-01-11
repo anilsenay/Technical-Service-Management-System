467fa8ec7147c0638daf96341911fb5fd3b003f9
bilgehangecici <bilgehangecici@gmail.com>
2021-01-11 21:48:44 +0300

    Update repairment.js

--- server/routes/repairment.js
+++ server/routes/repairment.js
@@ -39,6 +39,69 @@ router.get("/paid", (req, res) => {
     });
   });
 });
+// Get paid repairments.
+router.get("/getDetailedRepairments/:id", (req, res) => {
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("id", sql.Int, req.params.id);
+    request.execute("sp_getDetailedRepairments", (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code === "ENOCONN")
+          return res.status(503).send({ error: { err } });
+        return res.status(400).send({ error: { err } });
+      }
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+      const item = result.recordsets[0][0];
+      const allJson = {
+        ID: item.ID,
+        repairmentStartDate: item.repairmentStartDate,
+        repairmentEndDate: item.repairmentEndDate,
+        isPartWaited: item.isPartWaited,
+        isInWarranty: item.isInWarranty,
+        remark: item.remark,
+        repairmentDuration: item.repairmentDuration,
+        device: {
+          deviceID: item.deviceID,
+          model: item.model,
+          colorCode: item.colorCode,
+          serialCode: item.serialCode,
+          warrantyDueDate: item.warrantyDueDate,
+          warranty: item.warranty,
+          pyhsicalCondition: item.pyhsicalCondition,
+          proofOfPurchase: item.proofOfPurchase,
+        },
+        customer: {
+          customerID: item.customerID,
+          firstName: item.firstName,
+          lastName: item.lastName,
+        },
+        case: {
+          caseID: item.caseID,
+          caseType: item.caseType,
+          caseCategory: item.caseCategory,
+          caseSpecification: item.caseSpecification,
+          caseDescription: item.caseDescription,
+        },
+        employee: {
+          employeeID: item.employeeID,
+          EmpfirstName: item.EmpfirstName,
+          EmplastName: item.EmplastName,
+          username: item.username,
+          email: item.email,
+          isManager: item.isManager,
+          isSmartService: item.isSmartService,
+          isTechnician: item.isTechnician,
+          isStorageMan: item.isStorageMan,
+          isTester: item.isTester,
+          isAccountant: item.isAccountant,
+        },
+      };
+      return res.status(200).send({ detailedRepairment: allJson });
+    });
+  });
+});
 
 // Get repairments with given repairmentID.
 router.get("/:id", (req, res) => {
