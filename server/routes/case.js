8bf4d24865bf8e5c682561cabc5bc690a8a48ccb
bilgehangecici <bilgehangecici@gmail.com>
2021-01-12 14:51:11 +0300

    Update case.js

--- server/routes/case.js
+++ server/routes/case.js
@@ -22,6 +22,36 @@ router.get("/", (req, res) => {
   });
 });
 
+// Update the case.
+router.put("/update", (req, res) => {
+  var caseID = req.body.caseID;
+  var caseType = req.body.caseType;
+  var caseCategory = req.body.caseCategory;
+  var caseSpec = req.body.caseSpec;
+  var caseDesc = req.body.caseDesc;
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("caseID", sql.Int, caseID || null);
+    request.input("caseType", sql.TinyInt, caseType || null);
+    request.input("caseCategory", sql.TinyInt, caseCategory || null);
+    request.input("caseSpec", sql.TinyInt, caseSpec || null);
+    request.input("caseDesc", sql.NVarChar(100), caseDesc || "NULL");
+    request.execute("sp_updateCase", (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code === "ENOCONN")
+          return res.status(503).send({ error: { err } });
+        return res.status(400).send({ error: { err } });
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+
+      return res.status(200).send({ updatedCase: { ...req.body } });
+    });
+  });
+});
+
 // Insert a new case.
 router.post("/insert", (req, res) => {
   var caseType = req.body.caseType;
