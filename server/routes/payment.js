d8f6af68a94b402f8834e63a5b8c439e26776fa1
bilgehangecici <bilgehangecici@gmail.com>
2021-01-12 14:52:08 +0300

    Update payment.js

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -22,4 +22,54 @@ router.get("/", (req, res) => {
   });
 });
 
+// Insert a new payment.
+router.post("/insert", (req, res) => {
+  var repairmentID = req.body.repairmentID;
+  var accountantID = req.body.accountantID;
+  var totalCost = req.body.totalCost;
+  var paymentMethod = req.body.paymentMethod;
+
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("repairmentID", sql.Int, repairmentID || null);
+    request.input("accountantID", sql.TinyInt, accountantID || null);
+    request.input("totalCost", sql.SmallMoney, totalCost || null);
+    request.input("paymentMethod", sql.TinyInt, paymentMethod || null);
+    request.execute("sp_insertNewPayment", (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code === "ENOCONN")
+          return res.status(503).send({ error: { err } });
+        return res.status(400).send({ error: { err } });
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+
+      return res.status(200).send({ newPayment: { ...req.body } });
+    });
+  });
+});
+
+// Delete a new payment.
+router.delete("/delete/:paymentID", (req, res) => {
+  var paymentID = req.params.paymentID;
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("paymentID", sql.Int, paymentID || null);
+    request.execute("sp_deletePayment", (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code === "ENOCONN")
+          return res.status(503).send({ error: { err } });
+        return res.status(400).send({ error: { err } });
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+
+      return res.status(200).send({ deletedPayment: { ...req.params } });
+    });
+  });
+});
 module.exports = router;
