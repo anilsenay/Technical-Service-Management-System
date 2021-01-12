ded7f40b3d4c4a5bea6d59c9674ad8a07d2c133e
bilgehangecici <bilgehangecici@gmail.com>
2021-01-12 03:17:22 +0300

    Update storage.js

--- server/routes/storage.js
+++ server/routes/storage.js
@@ -22,6 +22,40 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/insertNewPart", (req, res) => {
+  var partID = req.body.partID;
+  var partName = req.body.partName;
+  var partModel = req.body.partModel;
+  var partColor = req.body.partColor;
+  var partPrice = req.body.partPrice;
+  var quantity = req.body.partColor;
+  var boxNumber = req.body.boxNumber;
+
+  sql.connect(sqlConfig, () => {
+    var request = new sql.Request();
+    request.input("partID", sql.BigInt, partID || null);
+    request.input("partName", sql.NVarChar(50), partName || "NULL");
+    request.input("partModel", sql.NVarChar(50), partModel || "NULL");
+    request.input("partColor", sql.NVarChar(100), partColor || "NULL");
+    request.input("partPrice", sql.SmallMoney, partPrice || null);
+    request.input("quantity", sql.SmallInt, quantity || null);
+    request.input("boxNumber", sql.SmallInt, boxNumber || null);
+    request.execute("sp_insertNewPartIntoStorage", (err, result) => {
+      if (err) {
+        console.log(err);
+        if (err.code === "ENOCONN")
+          return res.status(503).send({ error: { err } });
+        return res.status(400).send({ error: { err } });
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      sql.close();
+
+      return res.status(200).send({ newPart: { ...req.body } });
+    });
+  });
+});
+
 // Get part's quantity info with given quantity parameter.
 router.get("/parts/:lessThan", (req, res) => {
   sql.connect(sqlConfig, () => {
