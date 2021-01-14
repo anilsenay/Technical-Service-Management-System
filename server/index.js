11d7f080b1581ed2b510216486189f016ba95129
Anıl Şenay <anilsenay3@gmail.com>
2021-01-14 16:02:31 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.json());
